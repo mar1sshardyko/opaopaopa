4de873a5161a44a576f82c9ab135ac2642783d1f
maris <maris12345mimi@gmail.com>
2022-10-29 13:00:32 +0200

    не сдохнуть

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import random
 class Human:
-    def__init__(self, name = "Maris" , job = None , home = None , car = None) :
+    def __init__(self, name = "Maris" , job = None , home = None , car = None) :
          self.name = name
          self.job = job
          self.home = home
          self.car = car
          self.gladness = 50
-         self,stiety = 50
+         self.stiety = 50
+         def get_home(self):
+              pass
+         def get_car(self):
+              pass
+         def get_job(self):
+              pass
+         def get_home(self):
+              pass
+         def eat(self):
+              pass
+         def work(self):
+              pass
+         def shopping(self):
+              pass
+         def clear_home(self):
+              pass
