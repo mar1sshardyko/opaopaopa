ef6a371ac468f41216763b64b83b9d046628fa7d
maris <maris12345mimi@gmail.com>
2022-11-05 00:57:41 +0100

    доделал дз

--- main.py
+++ main.py
@@ -38,3 +38,73 @@ class Shadowfiend:
                    brands_of_cars = {"1soulult": {"damage":55, "soulsaway":1, "cosumption":55 },
                                                  "15soulult": {"damage": 111, "soulsaway": 15, "cosumption": 111},
                                                  "soulofcapybara": {"damage":999, "soulsaway":999, "cosumption":999 }}
+
+сlass Antimage:
+    def __init__(self, name = "Antimage" , blackkingbar = None , mantastyle = None , mannabreak = None) :
+         self.name = name
+         self.blackkingbar = blackkingbar
+         self.mantastyle = mantastyle
+         self.mannabreak = mannabreak
+         self.gladness = 50
+         self.stiety = 50
+         def get_mantastyle(self):
+              pass
+         def mannabreak(self):
+              pass
+         def get_blackkingbar(self):
+              pass
+         def get_mantastyle(self):
+              pass
+         def eat(self):
+              pass
+         def work(self):
+              pass
+         def shopping(self):
+              pass
+         def eat_mantastyle(self):
+              pass
+         def  day_indexes(self):
+              pass
+         def is_alive(self):
+              pass
+         def live(self):
+              pass
+         class Antimag_orno:
+              def __init__(self, brand_list):
+                   self.brand = random.choice(list(brand_list))
+                   self.fuel = brand_list[self.brand]["magicdefence1"]
+                   self.strenght = brand_list[self.brand]["magicdefence2"]
+                   self.cosumption = brand_list[self.brand]["magicdefence3"]
+                   brands_of_cars = {"tokskillpersa": {"magicdefence1":55, "magicdefence2":55, "magicdefence3":55 },
+                                                 "bkb": {"magicdefence1": 111, "magicdefence2": 111, "magicdefence3": 111},
+                                                 "defencecapybara": {"magicdefence1":999, "magicdefence2":999, "magicdefence3":999 }}
+
+if class Ultimate = '1soulult', class Antimage_orno = 'tokskillpersa'
+     print ("ничья")
+
+if class Ultimate = '15soulult', class Antimage_orno = 'tokskillpersa'
+     print ("Победа Шедоу Финда")
+
+if class Ultimate = 'soulofcapybara', class Antimage_orno = 'tokskillpersa'
+     print ("Победа Шедоу Финда")
+
+if class Ultimate = '1soulult', class Antimage_orno = 'bkb'
+     print ("Победа  Анти Мага")
+
+if class Ultimate = '1soulult', class Antimage_orno = 'defencecapybara'
+     print ("Победа  Анти Мага")
+
+if class Ultimate = '15soulult', class Antimage_orno = 'bkb'
+     print ("Ничья")
+
+if class Ultimate = 'soulofcapybara', class Antimage_orno = 'bkb'
+     print ("Победа Шедоу Финда")
+
+if class Ultimate = '15soulult', class Antimage_orno = 'bkb'
+     print ("Победа Шедоу Финда")
+
+if class Ultimate = '15soulult', class Antimage_orno = 'defencecapybara'
+     print ("Победа Антимага")
+
+if class Ultimate = 'soulofcapybara', class Antimage_orno = 'defencecapybara'
+     print ("Ничья")
